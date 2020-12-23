b14cd13c37e2d31fbafbeb3ee6aa9cbf97d35d56
yosuna@xyncro.net <adiel2008@gmail.com>
2020-12-23 18:00:52 -0500

    multitask

--- src/multitask.go
+++ src/multitask.go
@@ -2,8 +2,31 @@ package main
 
 import "fmt"
 
+type FactorialResult struct {
+	N, Result int
+}
+
+func factorial(n int, m chan FactorialResult) {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+	m <- FactorialResult{N: n, Result: result}
+}
+
 func MainMultitask() {
 	fmt.Println("==============================================================")
 	fmt.Println("Multitask")
 	fmt.Println("==============================================================")
+
+	var channels []chan FactorialResult
+	for _, v := range []int{33, 2, 12} {
+		ch := make(chan FactorialResult)
+		channels = append(channels, ch)
+		go factorial(v, ch)
+	}
+
+	for _, v := range channels {
+		fmt.Println(<-v)
+	}
 }
