3546f758848e8e741e178adf488969219681cb5a
yosuna@xyncro.net <adiel2008@gmail.com>
2020-12-23 17:22:09 -0500

    slice

--- src/slices.go
+++ src/slices.go
@@ -6,4 +6,18 @@ func MainSlices() {
 	fmt.Println("==============================================================")
 	fmt.Println("Slices")
 	fmt.Println("==============================================================")
+
+	var arr1 []int = []int{781, 22, 43, 94, 345, 346, 987, 8, 9}
+
+	slice1 := arr1[2:5]
+
+	for _, v := range slice1 {
+		fmt.Println(v)
+	}
+
+	mat1 := [3][4]int{{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}}
+
+	slice2 := mat1[1:3]
+	fmt.Println(slice2)
+
 }
